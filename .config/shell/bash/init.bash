62ebc4ae402741f34ea986cb0343c2b9b088f06d
mrjk <mrjk.78@gmail.com>
2023-04-03 18:08:47 -0400

    add: missing xsh init

---
+++ .config/shell/bash/init.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+#
+# This file is sourced automatically by xsh if the current shell is `bash`.
+#
+# It should merely register the modules to be loaded for each runcom:
+# env, login, interactive and logout.
+# The order in which the modules are registered defines the order in which
+# they will be loaded. Try `xsh help` for more information.
+#
+
+xsh module core
+
+# Load generic modules
+xsh module user
+case "$USER" in 
+  root)
+    xsh module admin
+    ;;
+esac
+
+# Load jez modules
+case "${SUDO_USER:-$USER}" in 
+  jez)
+    xsh module jez
+    xsh module ps1 
+    ;;
+esac
+
+#unset ${!_XSH_*}
