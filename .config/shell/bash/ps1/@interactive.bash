346eb142a12d74de50488271e0cffefc5b4d78ae
mrjk <mrjk.78@gmail.com>
2023-04-04 00:01:39 -0400

    wip

--- .config/shell/bash/ps1/@interactive.bash
+++ .config/shell/bash/ps1/@interactive.bash
@@ -2,7 +2,7 @@
 
 main ()
 {
-  local ps1_config=${_XSH_PS1_CONFIG:-default}
+  local ps1_config=${_XSH_PS1_CONFIG:-colors}
   
   if ! xsh load ps1 $ps1_config >&/dev/null; then
     _xsh_error "Failed to load PS1 '$ps1_config', fallback on default" -
